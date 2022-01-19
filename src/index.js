c7a85c8b439ef15860bd274df63f6af3bb91a65c
Tomas Sipko <t.sipko@student.tudelft.nl>
2022-01-19 08:27:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+import * as THREE from 'three';
+
+let app = {
+  el: document.getElementById("app"),
+  scene: null,
+  renderer: null,
+  camera: null
+}
+
+const init = () => {
+  app.renderer = new THREE.WebGLRenderer();
+  console.log(app.renderer);
+  app.renderer.setSize ( window.innerWidth, window.innerHeight);
+  app.el.appendChild (app.renderer.domElement);
+
+  app.scene = new THREE.Scene();
+
+  app.camera =  new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+};
+
+const render = () => {
+  requestAnimationFrame(render);
+  app.renderer.render(app.scene, app.camera);
+};
+
+init();
+render();
